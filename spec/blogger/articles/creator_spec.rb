eb8687af652a227fd495a8e06676351150c6d35c
Rafael George <george.rafael@gmail.com>
2014-01-28 19:33:29 -0400

    Fix failing spec regarding the way Blogger::Articles::Creator handles params keys

--- spec/blogger/articles/creator_spec.rb
+++ spec/blogger/articles/creator_spec.rb
@@ -2,7 +2,7 @@ require 'articles/creator'
 
 describe Blogger::Articles::Creator, '.create_article' do 
   let(:article_repo) { double :article } 
-  let(:attr) { {title:'article 1', body: 'article body'} }
+  let(:attr) { {'title' => 'article 1', 'body' => 'article body'} }
 
   it 'creates a new article' do 
     expect(article_repo).to receive(:create_article).with('article 1', 'article body')
